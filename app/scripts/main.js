122bf4968e12476d050d7217e035b4f7bf3ff7ad
every2ndcountsxc <jamesbarracca@yahoo.com>
2014-06-17 14:14:07 -0700

    lookbook

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+    'use strict';
+
+    $('a.is-toggle-login').on('click', function() {
+        $('#login-container').toggleClass('active');
+    });
+
+
+    $('.lookbook-item').on('click', function() {
+
+        if($(this).hasClass('active')) {
+            $('.lookbook-item').removeClass('active');
+            $('.lookbook-item').removeClass('inactive');
+        } else {
+            $('.lookbook-item').removeClass('active');
+            $('.lookbook-item').removeClass('inactive');
+            $(this).toggleClass('active');
+            $('.lookbook-item').not(this).toggleClass('inactive');
+        }
+    });
+
+
+});
+
