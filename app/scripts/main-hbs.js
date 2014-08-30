e51129f3b56201347289a59fd2f7490dc9a9b575
Riley Guerin <guerin.riley@gmail.com>
2014-08-29 18:26:22 -0700

    refactor with handelbars

--- app/scripts/main-hbs.js
+++ app/scripts/main-hbs.js
@@ -3,7 +3,24 @@ $(document).ready(function() {
     var template = Handlebars.compile(source);
 
     var html = template({
-        foo: 'bar'
+        modelName: 'Riley Richardson',
+        height: '5’7’’',
+        weight: '120',
+        shoe: '8',
+        dress: '2/4',
+        bust: '34C',
+        waist: '25',
+        hips: '34',
+        hair: 'Brown',
+        eyes: 'Blue',
+        instagram: 'thelifeof_ry'
     })
+    if(window.console) console.log("8:> " , html);
     $('#js-body').html(html)
+
+
+
+
+
+
 })
