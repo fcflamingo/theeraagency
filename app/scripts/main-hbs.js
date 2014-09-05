6d219f9a55a10e5a47718e2ddf12610f2acf211a
Riley Guerin <guerin.riley@gmail.com>
2014-09-05 14:26:58 -0700

    create a model page that is controlled by the hash

--- app/scripts/main-hbs.js
+++ app/scripts/main-hbs.js
@@ -5,19 +5,37 @@ $(document).ready(function() {
         var source   = $entryTemplate.html();
         var template = Handlebars.compile(source);
 
-        var html = template({
-            modelName: 'Riley Richardson',
-            height: '5’7’’',
-            weight: '120',
-            shoe: '8',
-            dress: '2/4',
-            bust: '34C',
-            waist: '25',
-            hips: '34',
-            hair: 'Brown',
-            eyes: 'Blue',
-            instagram: 'thelifeof_ry'
-        })
+        var models = [
+            {
+                pageName: 'riley-richardson',
+                modelName: 'Riley Richardson',
+                height: '5’7’’',
+                weight: '120',
+                shoe: '8',
+                dress: '2/4',
+                bust: '34C',
+                waist: '25',
+                hips: '34',
+                hair: 'Brown',
+                eyes: 'Blue',
+                instagram: 'thelifeof_ry'
+            }
+        ]
+
+        var findModelFromPage = function (models, pageName) {
+            return models.filter(function (item){
+                return item.pageName == pageName
+            })[0]
+        }
+        var getPageName = function () {
+            var hash = window.location.hash
+            return hash.slice(1, hash.length)
+        }
+
+
+        var data = findModelFromPage(models, getPageName())
+
+        var html = template(data)
 
         $('#js-body').html(html)
 
