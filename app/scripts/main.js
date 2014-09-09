877e92631537b0a63f1ecaefa467794adca4905f
every2ndcountsxc <jamesbarracca@yahoo.com>
2014-09-09 11:23:38 -0700

    adds code that was left out from older versions. fixes contact on desktop.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,5 +38,24 @@ $(document).ready(function() {
         });
     });
 
+    var contactOption = $('.contact-option');
+
+    var deactivateContactOptions = function () {
+        contactOption.removeClass('active');
+        contactOption.removeClass('inactive');
+    };
+
+    contactOption.on('click', function() {
+
+        if($(this).hasClass('active')) {
+            deactivateContactOptions();
+        }
+        else {
+            deactivateContactOptions();
+            $(this).toggleClass('active');
+            contactOption.not(this).toggleClass('inactive');
+        }
+    });
+
 });
 
