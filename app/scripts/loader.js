e72e02672d35b34d413bc8ae1d3af9fa68e975b6
Riley Guerin <guerin.riley@gmail.com>
2014-09-09 15:10:31 -0700

    put back safari loader for fear of it breaking (will handle in a different pull)

--- app/scripts/loader.js
+++ app/scripts/loader.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
     $('.loader-content').find('.line').addClass('active');
-    $('.loader').addClass('is-loaded');
+     $(window).on('load', function () {
+        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+            setTimeout(function () {
+                $('.loader').addClass('is-loaded');
+            }, 2000);
+        } else {
+            $('.loader').addClass('is-loaded');
+        }
+    });
 })
